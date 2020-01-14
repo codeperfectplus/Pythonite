6a7e226e205fd1c38f3a5aa3eadc5ad88a3cd3ea
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-01-14 19:15:05 +0530

    need to update

--- Python Projects/Rock-Paper_sciosors Game.py
+++ Python Projects/Rock-Paper_sciosors Game.py
@@ -1,10 +1,9 @@
 import random
-for i in range(1):
-    Qu = ["Rock", "Paper", "Sciosors"]
-    answer = random.choice(Qu)
-    print(answer)
+Qu = ["Rock", "Paper", "Sciosors"]
+answer = random.choice(Qu)
+print(answer)
 Ent = int(input("Enter to Start Game"))
-inp = int(input("Choose Your Number Below and Enter:\n 1.Rocl\n2.Paper\n3.Scisors\n"))
+inp = int(input("Choose Your Number Below and Enter:\n 1.Rock\n2.Paper\n3.Scisors\n"))
 if inp == answer:
     print("You won the Game")
 else:
